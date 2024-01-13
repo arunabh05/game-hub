36c50152bc0bd9613a38a85bff2966f3833a1eff
Arunabh Shrivastava <arunabh@MacBook-Pro.attlocal.net>
2024-01-13 14:34:25 -0800

    handle games with no image

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -2,7 +2,7 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
 interface Props {
-  sortOrder: string;
+  sortOrder: string | null;
   onSelectSortOrder: (sortOrder: string) => void;
 }
 
