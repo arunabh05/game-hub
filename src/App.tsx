568048c915e6541cae9378829f9a72504da2eb98
arunabh05 <arunabh@MacBook-Pro.attlocal.net>
2024-01-13 23:37:45 -0800

    remove vercel insight

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -64,7 +63,6 @@ function App() {
         </Box>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
-      <SpeedInsights />
     </Grid>
   );
 }
